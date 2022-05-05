54270bb6dd1f20d365472914c8eeb0f4e72495d1
crisptech <crisptech@hotmail.com>
2022-05-05 10:52:47 +0100

    fix: change uri to point to deployed server on heroku

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from "./pages";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  uri: "https://castronauts-server-app.herokuapp.com/",
   cache: new InMemoryCache(),
 });
 
